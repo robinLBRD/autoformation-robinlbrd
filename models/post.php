8937431b40e8b7a1b7007d51ee7275612930ee94
RobinL <robin.lbrd@eduge.ch>
2020-09-03 08:20:09 +0200

    Version 2.9
    mise en place de try catch

--- models/post.php
+++ models/post.php
@@ -7,7 +7,8 @@
   Classe : I.FDA.P3C
  */
 
-class Post {
+class Post
+{
 
     //déclaration de 3 variables public qui s'apparentent au champs de la base de donnée
     //le public permaet d'acceder à la valeur de la variable directement : $post->author
@@ -16,7 +17,8 @@ class Post {
     public $content;
 
     //constructeur
-    public function __construct($id, $author, $content) {
+    public function __construct($id, $author, $content)
+    {
         //utilisation de $this-> car les variables ne sont pas static
         $this->id = $id;
         $this->author = $author;
@@ -24,32 +26,43 @@ class Post {
     }
 
     //méthode qui permet d'afficher tout les pots
-    public static function all() {
+    public static function all()
+    {
         $list = [];
-        $db = Db::getInstance();//stockage de la connection à la base de donnée dans db
-        $req = $db->query('SELECT * FROM posts');//requete sql qui récupère tout les posts
-
-        foreach ($req->fetchAll() as $post) {//fetchAll = tableau contenant tout les enregistrements retournées par la requête
-            $list[] = new Post($post['id'], $post['author'], $post['content']);//ajout des posts dans la list
+        $db = Db::getInstance(); //stockage de la connection à la base de donnée dans db
+        try {
+            $req = $db->query('SELECT * FROM posts'); //requete sql qui récupère tout les posts
+            foreach ($req->fetchAll() as $post) { //fetchAll = tableau contenant tout les enregistrements retournées par la requête
+                $list[] = new Post($post['id'], $post['author'], $post['content']); //ajout des posts dans la list
+            }
+            //return de la list des posts
+            return $list;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            $db = null;
         }
-        //return de la list des posts
-        return $list;
     }
 
     //méthode qui permet d'afficher les informations d'un post précis
-    public static function find($id) {
+    public static function find($id)
+    {
         $db = Db::getInstance();
-        $validId = intval($id);//vérification de si $id est bien un chiffre
-        $req = $db->prepare('SELECT * FROM posts WHERE id = :id');//préparation d'une requête sql permettant de retrouver un post grâce à son id
-        $req->bindParam(':id', $validId);
-        $req->execute();//ajout dans la requête de la valeur de l'id
-        $post = $req->fetch();//récupération des infomrmations de l'enregistrement obtenu
-
-        return new Post($post['id'], $post['author'], $post['content']);//return du post au complet
+        $validId = intval($id); //vérification de si $id est bien un chiffre
+        try {
+            $req = $db->prepare('SELECT * FROM posts WHERE id = :id'); //préparation d'une requête sql permettant de retrouver un post grâce à son id
+            $req->bindParam(':id', $validId);
+            $req->execute(); //ajout dans la requête de la valeur de l'id
+            $post = $req->fetch(); //récupération des infomrmations de l'enregistrement obtenu
+            return new Post($post['id'], $post['author'], $post['content']); //return du post au complet
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            $db = null;
+        }
     }
 
     //méthode qui permet la création d'un post
-    public static function create() {
+    public static function create()
+    {
         $db = Db::getInstance();
         $message = null;
         //récupération des informations rentrées par l'utilisateur dans le formulaire de création 
@@ -57,16 +70,18 @@ class Post {
         $objet = filter_input(INPUT_POST, "objet", FILTER_SANITIZE_STRING);
         //a faire : vérifier le contenu des variables
         if (!empty($auteur) || !empty($objet)) {
-            $req = $db->prepare('INSERT INTO posts VALUES (null, :auteur, :objet)');//préparation de la requête sql pour ajouter une post dans la table
-            $req->bindParam(':auteur', $auteur);
-            $req->bindParam(':objet', $objet);//execution de la requête avec les bonnes valeurs
-            $req->execute();//execution de la requête avec les bonnes valeurs
-
-            $message = "Le post à bien été créé";
-            return $message;
+            try {
+                $req = $db->prepare('INSERT INTO posts VALUES (null, :auteur, :objet)'); //préparation de la requête sql pour ajouter une post dans la table
+                $req->bindParam(':auteur', $auteur);
+                $req->bindParam(':objet', $objet); //execution de la requête avec les bonnes valeurs
+                $req->execute(); //execution de la requête avec les bonnes valeurs
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                $db = null;
+            }
         }
     }
-    
+
     /*
     public static function update($id) {
         $db = Db::getInstance();
@@ -76,14 +91,19 @@ class Post {
         $post = $req->fetch();
         require_once('views/posts/update.php');
     }*/
-    
+
     //méthode qui permet de supprmier un post
-    public static function delete($id) {
+    public static function delete($id)
+    {
         $db = Db::getInstance();
-        $validId = intval($id);//vérification de si $id est bien un chiffre
-        $req = $db->prepare('DELETE FROM posts WHERE id = :id');//préparation de la requête
-        $req->bindParam(':id', $validId);
-        $req->execute();//execution de la requête avec l'id
+        $validId = intval($id); //vérification de si $id est bien un chiffre
+        try {
+            $req = $db->prepare('DELETE FROM posts WHERE id = :id'); //préparation de la requête
+            $req->bindParam(':id', $validId);
+            $req->execute(); //execution de la requête avec l'id
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            $db = null;
+        }
     }
 }
-?>
--- models/user.php
+++ models/user.php
@@ -7,7 +7,8 @@
   Classe : I.FDA.P3C
  */
 
-class User {
+class User
+{
 
     //déclaration de 3 variables public qui s'apparentent au champs de la base de donnée
     //le public permaet d'acceder à la valeur de la variable directement : $user->user
@@ -15,7 +16,8 @@ class User {
     public $user;
     public $pwd;
 
-    public function __construct($id, $user, $pwd) {
+    public function __construct($id, $user, $pwd)
+    {
         //utilisation de $this-> car les variables ne sont pas static
         $this->id = $id;
         $this->user = $user;
@@ -23,43 +25,34 @@ class User {
     }
 
     //méthode de vérification de l'éxistance du compte rentré par l'utilisateur
-    public static function verifyAcount() {
-        if (isset($_POST["valider"])) {//si l'utilisateur appuye sur le bouton validé
+    public static function verifyAcount()
+    {
+        if (isset($_POST["valider"])) { //si l'utilisateur appuye sur le bouton validé
             //récupération des valeurs de l'input
             $user = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
             $pwd = filter_input(INPUT_POST, "pwd", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
             $list = [];
             $db = Db::getInstance();
-            $req = $db->prepare('SELECT * FROM user WHERE user = :user AND pwd = :pwd'); //préparation de la requête de vérificaion
-            $req->bindParam(':user', $user);
-            $req->bindParam(':pwd', $pwd);
-            $req->execute(); //éxécution de la requête avec les bonnes valeurs
-            //renvoie de true ou false selon si le compte exsite ou non
-            $users = $req->fetch();
-            if ($users == false) {
-                //return false;
-                ?><p>Attention : ce compte n'éxiste pas ! Veuillez verifier le nom d'utilisateur ou le mot de passe rentré</p><?php
-            } else {
-                //return true;
-                ?><p>Vous êtes connecté(e) ! Bienvenue.</p><?php
-                /*
-                  if (isset($_SESSION)) {
-                  if (ini_get("session.use_cookies")) {
-                  $params = session_get_cookie_params();
-                  setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]
-                  );
-                  }
-                  session_destroy();
-                  session_start();
-                  }
-                  $_SESSION['logedIn'] = true;
-                  $_SESSION['username'] = $user;
-                  ?><p>Vous êtes connectés ! Bienvenue <?= $_SESSION['username'] ?>.</p><?php
-                 */
+            try {
+                $req = $db->prepare('SELECT * FROM user WHERE user = :user AND pwd = :pwd'); //préparation de la requête de vérificaion
+                $req->bindParam(':user', $user);
+                $req->bindParam(':pwd', $pwd);
+                $req->execute(); //éxécution de la requête avec les bonnes valeurs
+                //renvoie de true ou false selon si le compte exsite ou non
+                $users = $req->fetch();
+                if ($users == false) {
+                    //return false;
+                    ?><p>Attention : ce compte n'éxiste pas ! Veuillez verifier le nom d'utilisateur ou le mot de passe rentré</p><?php
+                } else {
+                    //return true;
+                    ?><p>Vous êtes connecté(e) ! Bienvenue.</p><?php
+                    }
+                } catch (PDOException $e) {
+                    echo $e->getMessage();
+                    $db = null;
+                }
             }
         }
     }
-
-}
-?>
+    ?>
